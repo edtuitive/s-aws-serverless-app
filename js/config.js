bbd8ce2c2312a6c78d9770c14c12a01847e0aac3
Mike DePalma <mike@edtuitive.com>
2020-06-30 10:43:52 -0400

    Updated login pool credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_M98QNB6U1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '666s1f7evp1oeuf12s326u7mgs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
